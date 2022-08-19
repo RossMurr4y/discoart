21eef8c576c95a152232260b7c638b0abecb9a2c
Ross Murray <rossmurray@protonmail.com>
2022-08-19 16:03:09 +1000

    feat: working first draft of schedules plotter

--- src/easing_functions/README.md
+++ src/easing_functions/README.md
@@ -4,6 +4,24 @@ I wanted a local script to produce the plots/charts/graphs that Disco Diffusion 
 
 This script does that.
 
+It reads an array of schedules from `./schedules.json` and produces a plot output to the `Path` attribute.
+
+## schedules.json reference
+
+```json
+[
+    {
+        "DisplayName":   "Title for Plot",
+        "Path":          "path/to/output/filename.png",
+        "cut_easing_fn": "CubicEaseInOut",
+        "cut_overview":  "[23]*100+dd_easing_int(start=23, end=1, steps=800, easing_fn=cut_easing_fn)+[1]*100",
+        "cut_innercut":  "[1]*100+dd_easing_int(start=1, end=23, steps=800, easing_fn=cut_easing_fn)+[23]*100",
+        "cut_ic_pow":    "dd_easing_float(start=10, end=1, steps=1000, easing_fn=cut_easing_fn)",
+        "cut_icgray_p":  "[0.6]*100+[0.2]*100+[0.1]*100+[0.05]*100+[0]*600"
+    }
+]
+```
+
 ## Install
 
 From this directory:
--- src/easing_functions/plot_easing_funcs.py
+++ src/easing_functions/plot_easing_funcs.py
@@ -1,8 +1,9 @@
 from easing_functions import *
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot
 import io
 import sys
+import json
 
 def easing_normalize(arr, t_min, t_max):
     norm_arr = []
@@ -11,11 +12,15 @@ def easing_normalize(arr, t_min, t_max):
     return norm_arr
 
 def dd_easing_int(start=1, end=9, steps=1000, easing_fn="ExponentialEaseInOut"): return list(np.rint(easing_normalize(list(map(eval(easing_fn + "(start=start, end=end, duration=steps)"), np.arange(0, steps, 1))), min([start, end]), max([start, end]))).astype(int))
-def easeint(st=1, en=9, s=1000, fn="ExponentialEaseInOut"): return dd_easing_int(start=en, end=en, steps=s, easing_fn=fn)
+def easeint(st=1, en=9, s=1000, fn="ExponentialEaseInOut"): return dd_easing_int(start=st, end=en, steps=s, easing_fn=fn)
 def dd_easing_float(start=1, end=9, steps=1000, easing_fn="ExponentialEaseInOut"): return list(easing_normalize(list(map(eval(easing_fn + "(start=start, end=end, duration=steps)"), np.arange(0, steps, 1))), min([start, end]), max([start, end])))
-def easeflt(st=1, en=9, s=1000, fn="ExponentialEaseInOut"): return dd_easing_float(start=st, end=e, steps=s, easing_fn=fn)
+def easeflt(st=1, en=9, s=1000, fn="ExponentialEaseInOut"): return dd_easing_float(start=st, end=en, steps=s, easing_fn=fn)
 
-def create_plot():
+def create_plot(title, path, cut_easing_fn, cut_overview, cut_innercut, cut_ic_pow):
+    plt = matplotlib.pyplot
+    plt.plot(np.arange(0, 1000, 1), eval(cut_overview), label='overview cuts')
+    plt.plot(np.arange(0, 1000, 1), eval(cut_innercut), label='inner cuts')   
+
     total_cuts = []
     cut_overview_evaled = eval(cut_overview)
     cut_innercut_evaled = eval(cut_innercut)
@@ -27,17 +32,26 @@ def create_plot():
     plt.plot(np.arange(0, 1000, 1), total_cuts, label='total cuts')
     plt.plot(np.arange(0, 1000, 1), eval(cut_ic_pow), label='ic_pow')
     plt.legend(loc='best')
-    plt.savefig('../../static/images/plots/cut_schedule_00.png')
-    print('Plot saved to ../../static/images/plots/cut_schedule_00.png')
+    plt.title(title)
+    plt.savefig(path)
+    print(f'Plot {name} saved to: {path}')
 
-cut_easing_fn = "CubicEaseInOut"
-cut_overview = "[23]*100+dd_easing_int(start=23, end=1, steps=800, easing_fn=cut_easing_fn)+[1]*100"
-cut_innercut = "[1]*100+dd_easing_int(start=1, end=23, steps=800, easing_fn=cut_easing_fn)+[23]*100"
-cut_ic_pow = "dd_easing_float(start=10, end=1, steps=1000, easing_fn=cut_easing_fn)"
-cut_icgray_p = "[0.6]*100+[0.2]*100+[0.1]*100+[0.05]*100+[0]*600"
+# open schedules.json
+with open('./schedules.json') as f:
+    data = json.load(f)
+    
+#cut_easing_fn = "CubicEaseInOut"
+#cut_overview = "[23]*100+dd_easing_int(start=23, end=1, steps=800, easing_fn=cut_easing_fn)+[1]*100"
+#cut_innercut = "[1]*100+dd_easing_int(start=1, end=23, steps=800, easing_fn=cut_easing_fn)+[23]*100"
+#cut_ic_pow = "dd_easing_float(start=10, end=1, steps=1000, easing_fn=cut_easing_fn)"
+#cut_icgray_p = "[0.6]*100+[0.2]*100+[0.1]*100+[0.05]*100+[0]*600"
 
-plt.plot(np.arange(0, 1000, 1), eval(cut_overview), label='overview cuts')
-plt.plot(np.arange(0, 1000, 1), eval(cut_innercut), label='inner cuts')
-
-# run
-create_plot()
+for plot in data:
+    name = plot['DisplayName']
+    path = plot['Path']
+    cut_easing_fn = plot['cut_easing_fn']
+    cut_overview = plot['cut_overview']
+    cut_innercut = plot['cut_innercut']
+    cut_ic_pow = plot['cut_ic_pow']
+    cut_icgray_p = plot['cut_icgray_p']
+    create_plot(name, path, cut_easing_fn, cut_overview, cut_innercut, cut_ic_pow)
